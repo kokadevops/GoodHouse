919213c19b1b859408ae817b3d08ec2e065e2665
Konstantin kokadevops <kokadevops@gmail.com>
2023-07-11 17:04:05 +0300

    add apollo-server and graphQL schema

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { schema } from "./graphql";
+
+const app = express();
+const port = 9000;
+
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app, path: "/api" });
+
+app.listen(port);
+
+console.log(`[app] : http://localhost:${port}`);
