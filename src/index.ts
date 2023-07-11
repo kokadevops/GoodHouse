7c3adeb95e2608ea2987a94e2d5838da9d031cf0
Konstantin kokadevops <kokadevops@gmail.com>
2023-07-11 19:13:26 +0300

    add typedefinitions and resolvers to schema

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
-import { schema } from "./graphql";
+import { typeDefs, resolvers } from "./graphql";
 
 const app = express();
 const port = 9000;
+const server = new ApolloServer({ typeDefs, resolvers });
 
-const server = new ApolloServer({ schema });
 server.applyMiddleware({ app, path: "/api" });
-
 app.listen(port);
 
-console.log(`[app] : http://localhost:${port}`);
+console.log(`[app] : http://localhost:${port}/api`);
